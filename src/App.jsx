36e3a25eff9c2562f7112ed6f844b4e8f1f8f5a5
sradusi <radusi.stefan@gmail.com>
2021-04-20 21:01:12 +0300

    add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header/Header";
 import { ToDoItem } from "./components/ToDoItem/ToDoItem";
+import { ToDoItemWithState } from "./components/ToDoItemWithState/ToDoItemWithState";
 
 import "./App.css";
 
@@ -7,7 +8,10 @@ function App() {
   return (
     <div className="App" id="app">
       <Header />
-      <ToDoItem />
+      <ToDoItem label="task 1" checkValue={false} />
+      <ToDoItem label="task 2" checkValue={true} />
+      <ToDoItem label="task 3" checkValue={true} />
+      <ToDoItemWithState label="task 4" checkValue={true} />
     </div>
   );
 }
--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -1,10 +1,12 @@
 import "./ToDoItem.css";
 
-export function ToDoItem() {
+export function ToDoItem(props) {
+  console.log(props);
+
   return (
     <div className="to-do-item">
-      <input type="checkbox" />
-      <p>Item</p>
+      <input type="checkbox" defaultChecked={props.checkValue} />
+      <p>{props.label}</p>
       <img
         src="https://previews.123rf.com/images/iconscart/iconscart1903/iconscart190300018/118811393-trash-icon.jpg"
         alt="trash"

063d4f157f4a6394a6a3109653bd93683a86c1c9
sradusi <radusi.stefan@gmail.com>
2021-04-25 12:21:07 +0300

    add comments

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -1,8 +1,16 @@
 import "./ToDoItem.css";
 
-export function ToDoItem(props) {
-  // console.log(props);
+// This is a function react component
+// by using props out reusable component can adapt to attributes passed in its parent component
+// to see how this component is used check ToDoList.jsx file
+// we use "{}" to evaluate values and interpolate them in our html objects
+// every classic react component (div, p, input, etc.) use event-like attribute (onClick, onChange, onMouseDown, etc) to link a function to an event
+// in props we can pass anything from strings, bool, number to objects and functions.
+// we pass a "removeItem" method from the parent component so that when user click on trash can we trigger the "removeItem" function that is defined in "ToDoList.jsx"
 
+// for more info in Component and Props: https://reactjs.org/docs/components-and-props.html
+
+export function ToDoItem(props) {
   return (
     <div className="to-do-item">
       <input type="checkbox" defaultChecked={props.checkValue} />
--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -16,13 +16,25 @@ const mockToDoListData = [
   },
 ];
 
+// "ToDoList" is a class component
+// we define class component when we need state, and the state can change during the lifetime of a component
+// we can use the internal state of a component to write logic inside render method so that when the state changes our component changes its rendered component as well
+// a component has a state so that we can have functionality in our apps, without state every component would be static
+
 export class ToDoList extends Component {
+  // here we declare the initial state
+  // our state will hold information regarding the list of to-do items as well as the current value in the add new item input
   state = {
     toDoList: mockToDoListData,
     inputValue: "",
   };
 
+  // removeItem is passed down to ToDoItem component and from there is triggered
+  // we need to define it here because when here is the state the we want to change
   removeItem = (itemText) => {
+    // "setState" will trigger a call of the render method of this component
+    // without setState the component would not update its rendered component and the user wouldn't see anything change in the webpage
+    // when we call setState we pass as an argument and object that will be merged to the current state
     this.setState({
       toDoList: this.state.toDoList.filter(
         (itemData) => itemData.item !== itemText

7a873468c7e6cc474047e2c17688bbc0c60a7f05
sradusi <radusi.stefan@gmail.com>
2021-04-27 18:43:44 +0300

    change trash url

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -16,7 +16,7 @@ export function ToDoItem(props) {
       <input type="checkbox" defaultChecked={props.checkValue} />
       <p>{props.label}</p>
       <img
-        src="https://previews.123rf.com/images/iconscart/iconscart1903/iconscart190300018/118811393-trash-icon.jpg"
+        src="https://image.freepik.com/free-icon/trash-bin-symbol_318-10194.jpg"
         alt="trash"
         onClick={(event) => props.removeItem(props.label)}
       />

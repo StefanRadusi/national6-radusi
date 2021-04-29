fab54863528d650741fe84b2de7f522aefe06110
sradusi <radusi.stefan@gmail.com>
2021-04-29 21:06:23 +0300

    add hooks

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,3 +1,5 @@
+import { Counter } from "../../components/Counter/Counter";
+
 export function About() {
   console.log("About render");
 
@@ -6,6 +8,7 @@ export function About() {
       <h2>To Do App Info</h2>
       <p>This app will store your task on server</p>
       <p>Fell free to add as many as you like on the home page</p>
+      <Counter />
     </div>
   );
 }

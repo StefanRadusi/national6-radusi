f05d3dc8e875264cf32f8c7490c276bee643683f
sradusi <radusi.stefan@gmail.com>
2021-04-29 19:58:56 +0300

    add react router

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import { AppLogo } from "../AppLogo/AppLogo";
+import { Link } from "react-router-dom";
+
 import "./Header.css";
 
 export function Header() {
@@ -6,6 +8,10 @@ export function Header() {
     <div className="app-header">
       <AppLogo />
       <p className="app-header__title">To Do Apppp</p>
+      <div className="navigation">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </div>
     </div>
   );
 }
